115672491696626f5c5f40d56e56741c85b3f67f
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-04-03 13:50:50 +0100

    branch this next time :|

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "kubernetes" {
 #  load_config_file = false
-  host = var.host
-  username = var.username
-  password = var.password
-  client_certificate = var.client_certificate
-  client_key = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
+  host = "${var.host}"
+  username = "${var.username}"
+  password = "${var.password}"
+  client_certificate = "${var.client_certificate}"
+  client_key = "${var.client_key}"
+  cluster_ca_certificate = "${var.cluster_ca_certificate}"
 }
 
 resource "kubernetes_secret" "default" {
   metadata {
     name = "env-secrets"
   }
-  data = data.external.json.result
+  data = "${data.external.json.result}"
 }
 
 # Use a ruby script to extract a json hash of environment variables
